db660c331ba9365b617724fb29f9e8faef2b36f4
Evan Krone <evankrone1@gmail.com>
2019-08-23 01:48:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 
 def get_japanese_emoticon(file_path,emot)
   emot_translator = load_library(file_path)
-  if emot_translator['get_emoticon'] do 
+  if emot_translator['get_emoticon'] 
     p emot
   else
     p"Sorry, that emoticon was not found"
