2f072b5f22ace0595ee18f98a5d7bc75915191df
Evan Krone <evankrone1@gmail.com>
2019-08-23 01:40:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 # require modules here
 
-def load_library(file)
+def load_library(file_path)
 require 'yaml'
-emots = YAML.load(File.read(file))
+emots = YAML.load(File.read(file_path))
 emot_hash = {'get_meaning' => {}, 'get_emoticon' => {}}
   emots.map do |key, value|
   english_emoticon = value[0]
@@ -14,8 +14,9 @@ emot_hash = {'get_meaning' => {}, 'get_emoticon' => {}}
 p emot_hash
 end
 
-def get_japanese_emoticon
-  
+def get_japanese_emoticon(file_path,emot)
+  emot_translator = load_library(file_path)
+  emot_translator[]
 end
 
 def get_english_meaning
