c7bda3d8c7dfca9a55584cefb10dd03997ad4cae
Evan Krone <evankrone1@gmail.com>
2019-08-23 01:54:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,10 +16,10 @@ end
 
 def get_japanese_emoticon(file_path,emot)
   emot_hash = load_library(file_path)
-  emot_hash['get_emoticon'].fetch(emoticon, "Sorry, that emoticon was not found") 
+  emot_hash['get_emoticon'].fetch(emot, "Sorry, that emoticon was not found") 
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emot)
  emot_hash = load_library(file_path)
-  emot_hash['get_meaning'].fetch(emoticon, "Sorry, that emoticon was not found") 
+  emot_hash['get_meaning'].fetch(emot, "Sorry, that emoticon was not found") 
 end
